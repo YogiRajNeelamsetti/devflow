5ebbd59a135d3363ba1303757fa1e59e1eec019f
NEELAMSETTI YOGIRAJ <yogineelamsetti@gmail.com>
2026-02-06 22:02:57 +0530

    Setup toggle light and dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import ThemeProvider from "@/context/ThemeProvider";
+import NavBar from "@/components/ui/navigation/navbar/NavBar";
 
 const inter = localFont({
   src: "./fonts/InterVF.ttf",
@@ -40,6 +41,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <NavBar />
           {children}
         </ThemeProvider>
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
+
 
 export default function Home() {
   return (
     <div>
-      <h1 className="h1-bold">Hi from App</h1>
       <h1 className="h1-bold font-space-grotesk">Hi from App</h1>
     </div>
   );
